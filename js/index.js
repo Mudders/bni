e18dbc30de7fbf184b361c8345e4b903c7b617f3
Jeremy Malherbe <jmalherbe@gmail.com>
2013-06-25 11:55:18 +0200

    5

--- js/index.js
+++ js/index.js
@@ -18,17 +18,20 @@
  */
 var app = {
     // Application Constructor
+    console.log("app 1")
     initialize: function() {
             this.bindEvents();
 
     },
-    
+    console.log("app 2")
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('load', this.onLoad, false);
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    console.log("app 3")
+        document.addEventListener('load', this.onLoad, false);console.log("app 4")
+        document.addEventListener('deviceready', this.onDeviceReady, false);console.log("app 5")
         window.addEventListener("orientationchange", orientationChange, true);
+        console.log("app 6")
     },
     onLoad: function() {
         
@@ -40,6 +43,6 @@ var app = {
             angular.bootstrap(document);
         });*/
         new FastClick(document.body);
-
+        console.log("app 7")
     }
 };
